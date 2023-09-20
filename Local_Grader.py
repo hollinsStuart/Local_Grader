cafa0f1940418bb001f28f3a3a0a51c99be3e10a
hollinsStuart <hollinsy@umich.edu>
2023-09-19 20:26:02 -0400

    DELETED

--- Local_Grader.py
+++ Local_Grader.py
@@ -1,26 +1,5 @@
-import os
 import subprocess
 
-if __name__ == '__main__':
+result = subprocess.run(["ls", "-l"], capture_output=True)
 
-    test_names = {'nor'}
-    run_diff = False
-
-    for name in test_names:
-        # get .as file
-        run = './assembler test_' + name + '.as test_' + name + '.mc'
-        # get answer
-        spec = './simulator test_' + name + '.mc > ' + name + '_ans.txt'
-        os.system(run)
-        os.system(spec)
-        subprocess.run(spec, capture_output=True)
-        # get correct answer
-        if run_diff:
-            # diff the results
-            diff = 'diff ' + name + '_ans.txt ' + 'name.correct'
-            subprocess.run(diff, capture_output=True)
-        # subprocess.check_output()
-
-
-
-    
+print(result.stdout)
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Local_Grader
+# Local Grader
+
+This is a Python program that allows you to run multiple local test cases for your code with only one line of code.
+
+```linux
+python3 Local_Grader.py
+```
+
+# Prerequisites
+
+## A ***Python 3*** interpreter in your environment
+
+Python 3 in **WSL** is recommended because we are running Linux commands. 
+
+
+
+# How to use
+
